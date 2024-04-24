fc14b74e5c7c45d5675cab6b1f9bc5f1a1d274d9
RuiYuChen1101 <rchen1@epsg.upv.es>
2024-04-24 11:13:40 +0200

    Movimiento de robot
    Readme actualizado, y reestructurado bien todo el proyecto entera

---
+++ Robot/install/proy_nexa_interface/include/proy_nexa_interface/msg/distvel.hpp
@@ -0,0 +1,11 @@
+// generated from rosidl_generator_cpp/resource/idl.hpp.em
+// generated code does not contain a copyright notice
+
+#ifndef PROY_NEXA_INTERFACE__MSG__DISTVEL_HPP_
+#define PROY_NEXA_INTERFACE__MSG__DISTVEL_HPP_
+
+#include "proy_nexa_interface/msg/detail/distvel__struct.hpp"
+#include "proy_nexa_interface/msg/detail/distvel__builder.hpp"
+#include "proy_nexa_interface/msg/detail/distvel__traits.hpp"
+
+#endif  // PROY_NEXA_INTERFACE__MSG__DISTVEL_HPP_
