4c02f02d10e27a6adff1315bc0b1ca842970bc3b
Arnau <clavileno3000@gmail.com>
2024-05-01 20:23:55 +0200

    Movimiento Web v1 - Botones
    Ya se puede realizar el movimiento del robot desde la web con una condición:
    Si se quiere en Gazebo, hay que comentar la linea "export ROS_DOMAIN_ID=5". De lo contrario, si se quiere en el real, la dejaremos
    
    Por el momento, cada dirección llama a su propio .bash para saber qué acción realizar. Pero en un futuro se planea comprimir en un archivo único move.bash

--- Servidor/comandos/movimiento/izquierda.bash
+++ Servidor/comandos/movimiento/izquierda.bash
@@ -1,3 +1,9 @@
 #Comando ROS2 para mover izquierda
-export ROS_DOMAIN_ID=5
+
+#Me situo en Robot
+cd ../../Robot
+source install/setup.bash
+
+#Exporto la variable ROS_DOMAIN
+#export ROS_DOMAIN_ID=5
 ros2 run proy_nexa_service proy_nexa_movement_client "izquierda"
--- Servidor/comandos/movimiento/move.bash
+++ Servidor/comandos/movimiento/move.bash
@@ -1,4 +1,4 @@
-#Comando ROS2 para mover atras
+#Comando ROS2 para detenerse
 
 #Me situo en Robot
 cd ../../Robot
@@ -6,4 +6,8 @@ source install/setup.bash
 
 #Exporto la variable ROS_DOMAIN
 #export ROS_DOMAIN_ID=5
-ros2 run proy_nexa_service proy_nexa_movement_client "atras"
+
+move(){
+	ros2 run proy_nexa_service proy_nexa_movement_client $1
+}
+
